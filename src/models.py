198864f59b2917662abb91ee3f1825cb5ada54d6
ProyectBlackWolf3005 <137832661+ProyectBlackWolf3005@users.noreply.github.com>
2026-05-25 07:12:19 +0000

    Mejorar modelo de datos del blog de StarWars

--- src/models.py
+++ src/models.py
@@ -17,17 +17,36 @@ class User(db.Model):
 
     favorites: Mapped[list["Favorite"]] = relationship(back_populates="user")
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "is_active": self.is_active
+        }
+
 
 class Planet(db.Model):
     __tablename__ = "planet"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(80), nullable=False)
-    climate: Mapped[str] = mapped_column(String(80))
-    terrain: Mapped[str] = mapped_column(String(80))
-    population: Mapped[str] = mapped_column(String(80))
+    climate: Mapped[str] = mapped_column(String(80), nullable=False)
+    terrain: Mapped[str] = mapped_column(String(80), nullable=False)
+    population: Mapped[str] = mapped_column(String(80), nullable=False)
 
+    characters: Mapped[list["Character"]] = relationship(back_populates="homeworld")
     favorites: Mapped[list["Favorite"]] = relationship(back_populates="planet")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "population": self.population
+        }
 
 
 class Character(db.Model):
@@ -35,13 +54,23 @@ class Character(db.Model):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(80), nullable=False)
-    gender: Mapped[str] = mapped_column(String(40))
-    hair_color: Mapped[str] = mapped_column(String(40))
-    eye_color: Mapped[str] = mapped_column(String(40))
-    planet_id: Mapped[int] = mapped_column(ForeignKey("planet.id"))
+    gender: Mapped[str] = mapped_column(String(40), nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(40), nullable=False)
+    eye_color: Mapped[str] = mapped_column(String(40), nullable=False)
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planet.id"), nullable=False)
 
-    homeworld: Mapped["Planet"] = relationship()
+    homeworld: Mapped["Planet"] = relationship(back_populates="characters")
     favorites: Mapped[list["Favorite"]] = relationship(back_populates="character")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            "planet_id": self.planet_id
+        }
 
 
 class Favorite(db.Model):
@@ -55,3 +84,11 @@ class Favorite(db.Model):
     user: Mapped["User"] = relationship(back_populates="favorites")
     planet: Mapped["Planet"] = relationship(back_populates="favorites")
     character: Mapped["Character"] = relationship(back_populates="favorites")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            "character_id": self.character_id
+        }
